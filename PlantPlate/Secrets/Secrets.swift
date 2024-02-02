0ec07560b5a973c331d1e10a9b9da8917bc109f3
JonesHenrik <henrikjones22@gmail.com>
2024-02-01 19:27:26 -0500

    Add .gitignore file

---
+++ PlantPlate/Secrets/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  PlantPlate
+//
+//  Created by Henrik Jones on 2/1/24.
+//
+
+import Foundation
+
+enum Secrets {
+    static let apiKey = "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"
+}
